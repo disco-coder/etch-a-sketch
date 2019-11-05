098664b5cabf9abecc88a50a701b6879c795bf80
disco-coder <1koblizek1@gmail.com>
2019-11-05 13:45:10 +0100

    Add random color and choose your color functionality, refactor code into functions

--- app.js
+++ app.js
@@ -1,30 +1,90 @@
+// Div containing the squares
 const container = document.querySelector("#container");
 
 // Create grid
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
-    }
-}
-
-const squares = document.querySelectorAll(".square");
+createGrid(16);
 
 // Make squares black on hover
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", (e) => {
-        square.style.backgroundColor = "black";
-    });
-});
+colorGrid("black");
 
-const button = document.querySelector("button");
-button.addEventListener("click", (e) => {
+// Choose your color button
+const chooseColorButton = document.querySelector("#chooseColorButton")
+chooseColorButton.addEventListener("click", () => {
+    let color = prompt("Choose your color:");
+    colorGrid(color);
+})
+
+// Random colors
+const randomColorButton = document.querySelector("#randomColorButton")
+randomColorButton.addEventListener("click", () => randomColorGrid())
+
+// Clear grid button
+const clearButton = document.querySelector("#clearButton");
+clearButton.addEventListener("click", () => clearGrid());
+
+// Resize grid button
+const resizeButton = document.querySelector("#resizeButton");
+resizeButton.addEventListener("click", (e) => {
+    
+    // Remove old grid
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+
+    // Prompt for grid size
+    do {
+        var gridSize = parseInt(prompt("Choose grid size:", "16"))
+    } while (isNaN(gridSize));
+
+    createGrid(gridSize);
+    colorGrid("black");
+})
+
+function clearGrid() {
+    let squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.style.backgroundColor = "white";
     });
-});
+}
+
+function colorGrid(color) {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", (e) => {
+            square.style.backgroundColor = color;
+        });
+    });
+}
+
+function randomColorGrid() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", (e) => {
+            square.style.backgroundColor = getRandomColor();
+        });
+    });
+}
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        
+        for (let j = 0; j < gridSize; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = `${(480/gridSize)}px`;
+            row.appendChild(square);
+        }
+    }
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
